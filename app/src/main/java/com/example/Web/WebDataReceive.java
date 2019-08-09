7546134c3eaa7b5871ccf8afff07283351be21ab
Dengyaohui <852376156@qq.com>
2019-08-09 15:43:52 +0800

    数据格式及uid有调整，数据量不再定死，数据量跟随后台回馈

--- app/src/main/java/com/example/Web/WebDataReceive.java
+++ app/src/main/java/com/example/Web/WebDataReceive.java
@@ -2,6 +2,8 @@ package com.example.Web;
 
 import android.util.Log;
 
+import com.example.Activity.GlobleVariable;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,7 +20,6 @@ import okhttp3.Response;
 import static com.example.MyThread.ClassReadPCMdata_From_Web.DataFromWeb;
 import static com.example.MyThread.ClassReadPCMdata_From_Web.create_time;
 import static com.example.MyThread.ClassReadPCMdata_From_Web.volet;
-import static java.lang.Float.parseFloat;
 import static java.lang.String.valueOf;
 
 
@@ -38,7 +39,7 @@ public class WebDataReceive {
 
 		//POST请求
 		FormBody formBody = new FormBody.Builder()
-				.add("uid", "1")
+				.add("uid", "001")
 				.build();
 
 		Request request = new Request.Builder()
@@ -66,23 +67,34 @@ public class WebDataReceive {
 					JSONObject jsonObject = new JSONObject(jsonString);
 					JSONArray jsonArray = jsonObject.getJSONArray("data");
 //						Log.i("data>>>>>>>>>>>>>>", String.valueOf(jsonArray));
-						JSONObject dataObject = (JSONObject) jsonArray.get(0);
-						String voletString = dataObject.getString("volet");
-						String voletStringArray[] = voletString.split(",");
-						for (int i = 0;i < voletStringArray.length;i ++){
-							volet[i] = parseFloat(voletStringArray[i])*10;
-						}
-						DataFromWeb[0] = dataObject.getString("V_rms");
-						DataFromWeb[1] = dataObject.getString("Plusevalue");
-						DataFromWeb[2] = dataObject.getString("Marginvalue");
-						DataFromWeb[3] = dataObject.getString("Ppeakvalue");
-						DataFromWeb[4] = dataObject.getString("Kurtuosis");
-						create_time = dataObject.getString("create_time");
+					JSONObject dataObject = (JSONObject) jsonArray.get(0);
+					String voletString = dataObject.getString("volet");
+					String voletStringArray[] = voletString.split(",");
+//					float[] Volet = new float[voletString.length()];
+					volet = new float[voletString.length()];
+					getVoletData(volet,voletStringArray);
+					DataFromWeb[0] = dataObject.getString("V_rms");
+					DataFromWeb[1] = dataObject.getString("Plusevalue");
+					DataFromWeb[2] = dataObject.getString("Marginvalue");
+					DataFromWeb[3] = dataObject.getString("Ppeakvalue");
+					DataFromWeb[4] = dataObject.getString("Kurtuosis");
+					create_time = dataObject.getString("create_time");
 				} catch (JSONException e) {
 					e.printStackTrace();
-					System.out.print(">>>>>>>>>>>Json数据解析错误!");
+					System.out.println("Json数据解析错误!>>>>>>>>>>>"+ GlobleVariable.ReceiveFailure);
+					GlobleVariable.ReceiveFailure = true;
 				}
 			}
 		});
+	}
+	private static void getVoletData(float[] volet, String[] voletStringArray){
+		for (int i = 0; i < voletStringArray.length; i++) {
+			try {
+				volet[i] = Float.parseFloat(voletStringArray[i]);
+			} catch (Exception e) {
+				volet[i] = 0.000f;
+				System.out.println("接收到不规范原始数据------->" + e.toString());
+			}
+		}
 	}
 }
